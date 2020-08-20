29830d8c573c9c0078952602ec5fb2a8cc4a10ef
Owais Islam <owaisislam@gmail.com>
2020-08-19 21:40:14 -0500

    created timer start on begin

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var body = document.querySelector("body");
+var timer = document.getElementById("timer");
+
+var beginButtonClick = document.querySelector("#beginButton");
+
+beginButtonClick.addEventListener("click", function () {
+    var seconds = 60,
+        mins = 1;
+
+    function tick() {
+        var counter = document.getElementById("timer");
+        var current_minutes = mins - 1
+        seconds--;
+        counter.innerHTML = "Time " + current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if (seconds > 0) {
+            setTimeout(tick, 1000);
+        } else {
+            if (mins > 1) {
+                countdown(mins - 1);
+            }
+        }
+        if (seconds < 21) {
+            timer.style.color = "red";
+        }
+    }
+    tick();
+})
